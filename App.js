a2665b462ff6657764254b5f795989c5730cb200
April Mintac Pineda <aprilmintacpineda@gmail.com>
2020-12-17 15:40:50 +0800

    added firebase app and firebase messaging;

--- App.js
+++ App.js
@@ -1,114 +1,54 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- * @flow strict-local
- */
-
 import React from 'react';
-import {
-  SafeAreaView,
-  StyleSheet,
-  ScrollView,
-  View,
-  Text,
-  StatusBar,
-} from 'react-native';
+import { Text } from 'react-native';
+import messaging from '@react-native-firebase/messaging';
+import iid from '@react-native-firebase/iid';
 
-import {
-  Header,
-  LearnMoreLinks,
-  Colors,
-  DebugInstructions,
-  ReloadInstructions,
-} from 'react-native/Libraries/NewAppScreen';
-
-const App: () => React$Node = () => {
-  return (
-    <>
-      <StatusBar barStyle="dark-content" />
-      <SafeAreaView>
-        <ScrollView
-          contentInsetAdjustmentBehavior="automatic"
-          style={styles.scrollView}>
-          <Header />
-          {global.HermesInternal == null ? null : (
-            <View style={styles.engine}>
-              <Text style={styles.footer}>Engine: Hermes</Text>
-            </View>
-          )}
-          <View style={styles.body}>
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>Step One</Text>
-              <Text style={styles.sectionDescription}>
-                Edit <Text style={styles.highlight}>App.js</Text> to change this
-                screen and then come back to see your edits.
-              </Text>
-            </View>
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>See Your Changes</Text>
-              <Text style={styles.sectionDescription}>
-                <ReloadInstructions />
-              </Text>
-            </View>
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>Debug</Text>
-              <Text style={styles.sectionDescription}>
-                <DebugInstructions />
-              </Text>
-            </View>
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>Learn More</Text>
-              <Text style={styles.sectionDescription}>
-                Read the docs to discover what to do next:
-              </Text>
-            </View>
-            <LearnMoreLinks />
-          </View>
-        </ScrollView>
-      </SafeAreaView>
-    </>
-  );
-};
-
-const styles = StyleSheet.create({
-  scrollView: {
-    backgroundColor: Colors.lighter,
-  },
-  engine: {
-    position: 'absolute',
-    right: 0,
-  },
-  body: {
-    backgroundColor: Colors.white,
-  },
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
-  },
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: '600',
-    color: Colors.black,
-  },
-  sectionDescription: {
-    marginTop: 8,
-    fontSize: 18,
-    fontWeight: '400',
-    color: Colors.dark,
-  },
-  highlight: {
-    fontWeight: '700',
-  },
-  footer: {
-    color: Colors.dark,
-    fontSize: 12,
-    fontWeight: '600',
-    padding: 4,
-    paddingRight: 12,
-    textAlign: 'right',
-  },
+messaging().setBackgroundMessageHandler(async remoteMessage => {
+  console.log('setBackgroundMessageHandler', remoteMessage);
 });
 
-export default App;
+function App () {
+  React.useEffect(() => {
+    (async () => {
+      const token = await iid().getToken();
+      console.log('token', token);
+
+      // getInitialNotification works fine and can be alternative
+      // for onNotificationOpenedApp
+      // const clickedNotif = await messaging().getInitialNotification();
+      // console.log('clickedNotif', clickedNotif);
+
+      const authStatus = await messaging().requestPermission();
+      const wasAllowed = (
+        authStatus === messaging.AuthorizationStatus.AUTHORIZED ||
+        authStatus === messaging.AuthorizationStatus.PROVISIONAL
+      );
+
+      if (!wasAllowed) return;
+
+      const subscribers = [
+        messaging().onMessage(async remoteMessage => {
+          console.log('onMessage', remoteMessage);
+        }),
+
+        messaging().onNotificationOpenedApp(async remoteMessage => {
+          console.log('onNotificationOpenedApp', remoteMessage);
+        }),
+
+        messaging().onTokenRefresh(async token => {
+          console.log('onTokenRefresh', token);
+        })
+      ];
+
+      return () => {
+        subscribers.forEach(unsubscribeCallback => {
+          if (unsubscribeCallback) unsubscribeCallback();
+        });
+      };
+    })();
+  }, []);
+
+  return <Text>App</Text>;
+}
+
+export default React.memo(App);
---
+++ README.md
@@ -0,0 +1,27 @@
+# Reproducible demo for react-native-firebase issue #194
+
+https://github.com/zoontek/react-native-bootsplash/issues/194
+
+# Getting started
+
+1. Clone repo
+2. Create firebase project
+3. Add Android and iOS app on that project
+4. Download the `Google-Services.plist` and `google-services.js` files.
+5. Replace the `ios/Google-Services.plist` and `android/app/google-services.js` files with the ones you downloaded respectively.
+6. cd to the clone repo.
+7. run `yarn install-all` which will do `yarn install && cd ios && pod install && cd ..`
+8. run `yarn start`
+8. Open XCode, and run the app on a real iphone device.
+9. Run the app on an android emulator or a real android device.
+10. Send notification while the app is on the foreground, you'll notice console logs when the device has received the notif.
+11. Send notification while the app is on killed state, you'll see notifications on the status bar of the device.
+12. Tap on the notification,
+
+expectation is that `onNotificationOpened` will be called, but it won't be called.
+
+# Work around
+
+use `messaging().getInitialNotification`.
+
+[Checkout diff]()
